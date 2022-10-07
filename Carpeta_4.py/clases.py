46edf99a8c0ed5458126fa54a0b3db03f514d491
acasasaez <acasasae@myuax.com>
2022-10-07 16:58:18 +0200

    clase, constructor, set, get

---
+++ Carpeta_4.py/clases.py
@@ -0,0 +1,28 @@
+class producto ():
+    def __init__ (self, codigo, nombre, precio, tipo):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.precio = precio
+        self.tipo = tipo
+
+    def set_codigo(self, codigo):
+        self.codigo = codigo
+    def get_codigo(self):
+        return self.codigo
+
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+    def get_nombre(self):
+        return self.nombre
+
+    def set_precio(self, precio):
+        self.precio = precio
+    def get_precio(self):
+        return self.precio
+
+    def set_tipo(self, tipo):
+        self.tipo = tipo
+    def get_tipo(self):
+        return self.tipo
+
+    

39d552a99c295f94911f774257f7fea364854d13
acasasaez <acasasae@myuax.com>
2022-10-07 15:49:13 +0200

    creamos carpetas para diferentes ejercicios
    Ademas he empezado ya el ejercicio 1

---
+++ Carpeta_2.py/clases_2.py
@@ -0,0 +1,12 @@
+class Alumno():
+    def __init__(self, nombre,nota):
+        self.nombre = nombre
+        self.nota = nota
+    
+    def __str__(self):
+        return "Nombre: {}, Nota: {}".format(self.nombre, self.nota)
+
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+    def get_nombre(self):
+        return self.nombre

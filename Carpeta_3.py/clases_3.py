028a4ab1ef713cde1606bf7e10e5fc9e3989195f
acasasaez <acasasae@myuax.com>
2022-10-07 16:48:37 +0200

    copiamos codigo y añadimos str

--- Carpeta_3.py/clases_3.py
+++ Carpeta_3.py/clases_3.py
@@ -1,1 +1,27 @@
-lases
+class Alumno():
+    def __init__(self, nombre,nota):
+        self.nombre = nombre
+        self.nota = nota
+    
+    def __str__(self):
+        return "Nombre: {}, Nota: {}".format(self.nombre, self.nota)
+
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+    def get_nombre(self):
+        return self.nombre
+
+    def set_nota(self, nota):
+        self.nota = nota 
+    def get_nota(self):
+        return self.nota
+
+    def notas(self):
+        if self.nota < 5:
+            print("Suspenso") 
+        elif self.nota < 7:
+            print ("Aprobado")
+        elif self.nota < 9:
+            print ("Notable")
+        else:
+            print ("Sobresaliente")

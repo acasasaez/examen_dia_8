5ef6516655317d0314f71bf305a580fc318a9272
acasasaez <acasasae@myuax.com>
2022-10-07 16:26:39 +0200

    creando lanzador para main

--- Carpeta_2.py/lanzador.py
+++ Carpeta_2.py/lanzador.py
@@ -1,25 +1,29 @@
 import clases_2
-Maria = clases_2.Alumno()
-Juan = clases_2.Alumno()
-Ruben = clases_2.Alumno()
+Maria = clases_2.Alumno("Maria", 8)
+Juan = clases_2.Alumno("Juan", 6)
+Ruben = clases_2.Alumno("Ruben", 10)
 
-alumno_1.set_nombre("Maria")
-alumno_1.set_nota(7)
-alumno_2.set_nombre("Juan")
-alumno_2.set_nota(4)
-alumno_3.set_nombre("Ruben")
-alumno_3.set_nota(10)
+
 
 def notas(alumno):
     if alumno.get_nota() < 5:
-        return "Suspenso"
+        print("Suspenso") 
     elif alumno.get_nota() < 7:
-        return "Aprobado"
+        print ("Aprobado")
     elif alumno.get_nota() < 9:
-        return "Notable"
+        print ("Notable")
     else:
-        return "Sobresaliente"
+        print ("Sobresaliente")
 
 def inicio():
-    print("
-
+    print("Si quieres saber la nota de un alumno, introduce su nombre")
+    nombre = input(">")
+    if nombre == Maria.get_nombre():
+        notas(Maria)
+    elif nombre == Juan.get_nombre():
+        notas(Juan)
+    elif nombre == Ruben.get_nombre():
+        notas(Ruben)
+    else:
+        print("No se ha encontrado el alumno")
+        inicio()
